17c06ad3368ee475c7aa17560d296454f181b7c2
HaMZAAsif043 <asifnaseer043@gmail.com>
2024-11-30 21:22:37 +0500

    bug fixed

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ const Home = () => {
         <section className="w-full flex-center flex-col">
             <h1 className="head_text text-center">
                 Discover & Share Prompts
-                <br className="max-md:hiddden" />
-                <span className="orange_gradient text-center">AI -Powered Prompts</span>
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">AI-Powered Prompts</span>
             </h1>
             <p className="desc text-center">
                 Prompts Sharing is a platform where you can discover and share prompts for your writing, drawing, or any other creative work. Our AI-powered system generates prompts based on your preferences and helps you to overcome writer's block.
